b6ae08e8593452108c6fb7a4ee2402b577df8229
root <root@DESKTOP-NVLK686.localdomain>
2021-04-06 11:07:54 +0700

    Initial Commit

---
+++ TrafficControler/TrafficControler/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿
+namespace TrafficControler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTime1 = new System.Windows.Forms.TextBox();
+            this.btnG1 = new System.Windows.Forms.Button();
+            this.btnY1 = new System.Windows.Forms.Button();
+            this.btnR1 = new System.Windows.Forms.Button();
+            this.btnR2 = new System.Windows.Forms.Button();
+            this.btnY2 = new System.Windows.Forms.Button();
+            this.btnG2 = new System.Windows.Forms.Button();
+            this.btnG3 = new System.Windows.Forms.Button();
+            this.btnY3 = new System.Windows.Forms.Button();
+            this.btnR3 = new System.Windows.Forms.Button();
+            this.btnG4 = new System.Windows.Forms.Button();
+            this.btnY4 = new System.Windows.Forms.Button();
+            this.btnR4 = new System.Windows.Forms.Button();
+            this.txtTime2 = new System.Windows.Forms.TextBox();
+            this.txtTime3 = new System.Windows.Forms.TextBox();
+            this.txtTime4 = new System.Windows.Forms.TextBox();
+            this.txtTimeGreen = new System.Windows.Forms.TextBox();
+            this.Green = new System.Windows.Forms.Button();
+            this.YELLOW = new System.Windows.Forms.Button();
+            this.txtTimeYellow = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtTime1
+            // 
+            this.txtTime1.Enabled = false;
+            this.txtTime1.Font = new System.Drawing.Font("Product Sans Black", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtTime1.Location = new System.Drawing.Point(504, 241);
+            this.txtTime1.Multiline = true;
+            this.txtTime1.Name = "txtTime1";
+            this.txtTime1.Size = new System.Drawing.Size(125, 65);
+            this.txtTime1.TabIndex = 0;
+            this.txtTime1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTime1.TextChanged += new System.EventHandler(this.txtTime1_TextChanged);
+            // 
+            // btnG1
+            // 
+            this.btnG1.Enabled = false;
+            this.btnG1.Location = new System.Drawing.Point(433, 170);
+            this.btnG1.Name = "btnG1";
+            this.btnG1.Size = new System.Drawing.Size(65, 65);
+            this.btnG1.TabIndex = 1;
+            this.btnG1.UseVisualStyleBackColor = true;
+            // 
+            // btnY1
+            // 
+            this.btnY1.Enabled = false;
+            this.btnY1.Location = new System.Drawing.Point(433, 241);
+            this.btnY1.Name = "btnY1";
+            this.btnY1.Size = new System.Drawing.Size(65, 65);
+            this.btnY1.TabIndex = 2;
+            this.btnY1.UseVisualStyleBackColor = true;
+            // 
+            // btnR1
+            // 
+            this.btnR1.Enabled = false;
+            this.btnR1.Location = new System.Drawing.Point(433, 312);
+            this.btnR1.Name = "btnR1";
+            this.btnR1.Size = new System.Drawing.Size(65, 65);
+            this.btnR1.TabIndex = 3;
+            this.btnR1.UseVisualStyleBackColor = true;
+            // 
+            // btnR2
+            // 
+            this.btnR2.Enabled = false;
+            this.btnR2.Location = new System.Drawing.Point(220, 382);
+            this.btnR2.Name = "btnR2";
+            this.btnR2.Size = new System.Drawing.Size(65, 65);
+            this.btnR2.TabIndex = 6;
+            this.btnR2.UseVisualStyleBackColor = true;
+            // 
+            // btnY2
+            // 
+            this.btnY2.Enabled = false;
+            this.btnY2.Location = new System.Drawing.Point(291, 382);
+            this.btnY2.Name = "btnY2";
+            this.btnY2.Size = new System.Drawing.Size(65, 65);
+            this.btnY2.TabIndex = 5;
+            this.btnY2.UseVisualStyleBackColor = true;
+            // 
+            // btnG2
+            // 
+            this.btnG2.Enabled = false;
+            this.btnG2.Location = new System.Drawing.Point(362, 382);
+            this.btnG2.Name = "btnG2";
+            this.btnG2.Size = new System.Drawing.Size(65, 65);
+            this.btnG2.TabIndex = 4;
+            this.btnG2.UseVisualStyleBackColor = true;
+            // 
+            // btnG3
+            // 
+            this.btnG3.Enabled = false;
+            this.btnG3.Location = new System.Drawing.Point(151, 312);
+            this.btnG3.Name = "btnG3";
+            this.btnG3.Size = new System.Drawing.Size(65, 65);
+            this.btnG3.TabIndex = 9;
+            this.btnG3.UseVisualStyleBackColor = true;
+            // 
+            // btnY3
+            // 
+            this.btnY3.Enabled = false;
+            this.btnY3.Location = new System.Drawing.Point(151, 241);
+            this.btnY3.Name = "btnY3";
+            this.btnY3.Size = new System.Drawing.Size(65, 65);
+            this.btnY3.TabIndex = 8;
+            this.btnY3.UseVisualStyleBackColor = true;
+            // 
+            // btnR3
+            // 
+            this.btnR3.Enabled = false;
+            this.btnR3.Location = new System.Drawing.Point(151, 170);
+            this.btnR3.Name = "btnR3";
+            this.btnR3.Size = new System.Drawing.Size(65, 65);
+            this.btnR3.TabIndex = 7;
+            this.btnR3.UseVisualStyleBackColor = true;
+            // 
+            // btnG4
+            // 
+            this.btnG4.Enabled = false;
+            this.btnG4.Location = new System.Drawing.Point(220, 99);
+            this.btnG4.Name = "btnG4";
+            this.btnG4.Size = new System.Drawing.Size(65, 65);
+            this.btnG4.TabIndex = 12;
+            this.btnG4.UseVisualStyleBackColor = true;
+            // 
+            // btnY4
+            // 
+            this.btnY4.Enabled = false;
+            this.btnY4.Location = new System.Drawing.Point(291, 99);
+            this.btnY4.Name = "btnY4";
+            this.btnY4.Size = new System.Drawing.Size(65, 65);
+            this.btnY4.TabIndex = 11;
+            this.btnY4.UseVisualStyleBackColor = true;
+            // 
+            // btnR4
+            // 
+            this.btnR4.Enabled = false;
+            this.btnR4.Location = new System.Drawing.Point(362, 99);
+            this.btnR4.Name = "btnR4";
+            this.btnR4.Size = new System.Drawing.Size(65, 65);
+            this.btnR4.TabIndex = 10;
+            this.btnR4.UseVisualStyleBackColor = true;
+            // 
+            // txtTime2
+            // 
+            this.txtTime2.Enabled = false;
+            this.txtTime2.Font = new System.Drawing.Font("Product Sans Black", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtTime2.Location = new System.Drawing.Point(257, 453);
+            this.txtTime2.Multiline = true;
+            this.txtTime2.Name = "txtTime2";
+            this.txtTime2.Size = new System.Drawing.Size(125, 65);
+            this.txtTime2.TabIndex = 13;
+            this.txtTime2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTime2.TextChanged += new System.EventHandler(this.txtTime2_TextChanged);
+            // 
+            // txtTime3
+            // 
+            this.txtTime3.Enabled = false;
+            this.txtTime3.Font = new System.Drawing.Font("Product Sans Black", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtTime3.Location = new System.Drawing.Point(20, 241);
+            this.txtTime3.Multiline = true;
+            this.txtTime3.Name = "txtTime3";
+            this.txtTime3.Size = new System.Drawing.Size(125, 65);
+            this.txtTime3.TabIndex = 14;
+            this.txtTime3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTime3.TextChanged += new System.EventHandler(this.txtTime3_TextChanged);
+            // 
+            // txtTime4
+            // 
+            this.txtTime4.Enabled = false;
+            this.txtTime4.Font = new System.Drawing.Font("Product Sans Black", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtTime4.Location = new System.Drawing.Point(257, 28);
+            this.txtTime4.Multiline = true;
+            this.txtTime4.Name = "txtTime4";
+            this.txtTime4.Size = new System.Drawing.Size(125, 65);
+            this.txtTime4.TabIndex = 15;
+            this.txtTime4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTime4.TextChanged += new System.EventHandler(this.txtTime4_TextChanged);
+            // 
+            // txtTimeGreen
+            // 
+            this.txtTimeGreen.Font = new System.Drawing.Font("Product Sans Black", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtTimeGreen.Location = new System.Drawing.Point(748, 99);
+            this.txtTimeGreen.Multiline = true;
+            this.txtTimeGreen.Name = "txtTimeGreen";
+            this.txtTimeGreen.Size = new System.Drawing.Size(102, 50);
+            this.txtTimeGreen.TabIndex = 16;
+            this.txtTimeGreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTimeGreen.TextChanged += new System.EventHandler(this.txtTimeGreen_TextChanged);
+            // 
+            // Green
+            // 
+            this.Green.BackColor = System.Drawing.Color.Lime;
+            this.Green.Enabled = false;
+            this.Green.Location = new System.Drawing.Point(692, 99);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(50, 50);
+            this.Green.TabIndex = 17;
+            this.Green.UseVisualStyleBackColor = false;
+            // 
+            // YELLOW
+            // 
+            this.YELLOW.BackColor = System.Drawing.Color.Yellow;
+            this.YELLOW.Enabled = false;
+            this.YELLOW.Location = new System.Drawing.Point(692, 155);
+            this.YELLOW.Name = "YELLOW";
+            this.YELLOW.Size = new System.Drawing.Size(50, 50);
+            this.YELLOW.TabIndex = 19;
+            this.YELLOW.UseVisualStyleBackColor = false;
+            // 
+            // txtTimeYellow
+            // 
+            this.txtTimeYellow.Font = new System.Drawing.Font("Product Sans Black", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtTimeYellow.Location = new System.Drawing.Point(748, 155);
+            this.txtTimeYellow.Multiline = true;
+            this.txtTimeYellow.Name = "txtTimeYellow";
+            this.txtTimeYellow.Size = new System.Drawing.Size(102, 50);
+            this.txtTimeYellow.TabIndex = 18;
+            this.txtTimeYellow.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTimeYellow.TextChanged += new System.EventHandler(this.txtTimeYellow_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnStart.Location = new System.Drawing.Point(649, 432);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 60);
+            this.btnStart.TabIndex = 20;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // btnStop
+            // 
+            this.btnStop.ForeColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(779, 432);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(102, 60);
+            this.btnStop.TabIndex = 21;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 534);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.YELLOW);
+            this.Controls.Add(this.txtTimeYellow);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.txtTimeGreen);
+            this.Controls.Add(this.txtTime4);
+            this.Controls.Add(this.txtTime3);
+            this.Controls.Add(this.txtTime2);
+            this.Controls.Add(this.btnG4);
+            this.Controls.Add(this.btnY4);
+            this.Controls.Add(this.btnR4);
+            this.Controls.Add(this.btnG3);
+            this.Controls.Add(this.btnY3);
+            this.Controls.Add(this.btnR3);
+            this.Controls.Add(this.btnR2);
+            this.Controls.Add(this.btnY2);
+            this.Controls.Add(this.btnG2);
+            this.Controls.Add(this.btnR1);
+            this.Controls.Add(this.btnY1);
+            this.Controls.Add(this.btnG1);
+            this.Controls.Add(this.txtTime1);
+            this.Font = new System.Drawing.Font("Product Sans Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "TrafficSystem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTime1;
+        private System.Windows.Forms.Button btnG1;
+        private System.Windows.Forms.Button btnY1;
+        private System.Windows.Forms.Button btnR1;
+        private System.Windows.Forms.Button btnR2;
+        private System.Windows.Forms.Button btnY2;
+        private System.Windows.Forms.Button btnG2;
+        private System.Windows.Forms.Button btnG3;
+        private System.Windows.Forms.Button btnY3;
+        private System.Windows.Forms.Button btnR3;
+        private System.Windows.Forms.Button btnG4;
+        private System.Windows.Forms.Button btnY4;
+        private System.Windows.Forms.Button btnR4;
+        private System.Windows.Forms.TextBox txtTime2;
+        private System.Windows.Forms.TextBox txtTime3;
+        private System.Windows.Forms.TextBox txtTime4;
+        private System.Windows.Forms.TextBox txtTimeGreen;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Button RED;
+        private System.Windows.Forms.TextBox txtTimeRed;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button YELLOW;
+        private System.Windows.Forms.TextBox txtTimeYellow;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ TrafficControler/TrafficControler/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficControler
+{
+    public partial class Form1 : Form
+    {
+        int counter1 = 0;
+        int counter2 = 0;
+        int counter3 = 0;
+        int counter4 = 0;
+        int green;
+        int yellow;
+
+        int t1;
+        int t2;
+        int t3;
+        int t4;
+        int ans;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtTime4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            counter1++;
+            if (counter1 < green)
+            {
+                btnG1.BackColor = Color.Lime;
+                btnR2.BackColor = Color.Red;
+                btnR3.BackColor = Color.Red;
+                btnR4.BackColor = Color.Red;
+                
+            }
+            else if (counter1 < (green + yellow))
+            {
+                btnY1.BackColor = Color.Yellow;
+                btnG1.BackColor = Color.FromArgb(224, 224, 224);
+                btnR2.BackColor = Color.Red;
+                btnR3.BackColor = Color.Red;
+                btnR4.BackColor = Color.Red;
+            }
+            else if (counter1 == (green + yellow))
+            {
+                btnR1.BackColor = Color.Red;
+                btnY1.BackColor = Color.FromArgb(224, 224, 224);
+                btnR2.BackColor = Color.Red;
+                btnR3.BackColor = Color.Red;
+                btnR4.BackColor = Color.Red;
+            }
+
+            else if (counter1 > (green + yellow))
+            {
+                counter2++;
+                if (counter2 < green)
+                {
+                    btnG2.BackColor = Color.Lime;
+                    btnR2.BackColor = Color.FromArgb(224, 224, 224); 
+                    btnR1.BackColor = Color.Red;
+                    btnR3.BackColor = Color.Red;
+                    btnR4.BackColor = Color.Red;
+                 
+                }
+                else if (counter2 < (green + yellow))
+                {
+                    btnY2.BackColor = Color.Yellow;
+                    btnG2.BackColor = Color.FromArgb(224, 224, 224);
+
+                    btnR1.BackColor = Color.Red;
+                    btnR3.BackColor = Color.Red;
+                    btnR4.BackColor = Color.Red;
+
+                }
+                else if (counter2 == (green + yellow))
+                {
+                    btnR2.BackColor = Color.Red;
+                    btnY2.BackColor = Color.FromArgb(224, 224, 224);
+
+                    btnR1.BackColor = Color.Red;
+                    btnR2.BackColor = Color.Red;
+                    btnR4.BackColor = Color.Red;
+
+                }
+
+                else if (counter2 > (green + yellow))
+                {
+                    counter3++;
+                    if (counter3 < green)
+                    {
+                        btnG3.BackColor = Color.Lime;
+                        btnR3.BackColor = Color.FromArgb(224, 224, 224);
+
+                        btnR1.BackColor = Color.Red;
+                        btnR2.BackColor = Color.Red;
+                        btnR4.BackColor = Color.Red;
+                    }
+                    else if (counter3 < (green + yellow))
+                    {
+                        btnY3.BackColor = Color.Yellow;
+                        btnG3.BackColor = Color.FromArgb(224, 224, 224);
+
+                        btnR1.BackColor = Color.Red;
+                        btnR2.BackColor = Color.Red;
+                        btnR4.BackColor = Color.Red;
+
+                    }
+                    else if (counter3 == (green + yellow))
+                    {
+                        btnR3.BackColor = Color.Red;
+                        btnY3.BackColor = Color.FromArgb(224, 224, 224);
+                        btnR1.BackColor = Color.Red;
+                        btnR2.BackColor = Color.Red;
+                        btnR4.BackColor = Color.Red;
+                    }
+                    else if (counter3 > (green + yellow))
+                    {
+                        counter4++;
+                        if (counter4 < green)
+                        {
+                            btnG4.BackColor = Color.Lime;
+                            btnR4.BackColor = Color.FromArgb(224, 224, 224);
+
+                            btnR1.BackColor = Color.Red;
+                            btnR2.BackColor = Color.Red;
+                            btnR3.BackColor = Color.Red;
+                        }
+                        else if (counter4 < (green + yellow))
+                        {
+                            btnY4.BackColor = Color.Yellow;
+                            btnG4.BackColor = Color.FromArgb(224, 224, 224);
+
+                            btnR1.BackColor = Color.Red;
+                            btnR2.BackColor = Color.Red;
+                            btnR3.BackColor = Color.Red;
+
+                        }
+                        else if (counter4 == (green + yellow))
+                        {
+                            btnR4.BackColor = Color.Red;
+                            btnY4.BackColor = Color.FromArgb(224, 224, 224);
+                            btnR2.BackColor = Color.Red;
+                            btnR4.BackColor = Color.Red;
+                            btnR1.BackColor = Color.FromArgb(224, 224, 224);
+                            btnG1.BackColor = Color.Lime;
+                        }
+                         else
+                        {
+                            counter1 = 1;
+                            counter2 = 0;
+                            counter3 = 0;
+                            counter4 = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+
+
+        private void txtTimeGreen_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtTimeYellow_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+            timer5.Enabled = false;
+        }
+
+        private void txtTime1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = true;
+            timer5.Enabled = true;
+            green = Convert.ToInt32(txtTimeGreen.Text);
+            yellow = Convert.ToInt32(txtTimeYellow.Text);
+            ans = green + yellow;
+            t1 = ans * 4;
+            t2 = ans * 4;
+            t3 = ans * 4;
+            t4 = ans * 4;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            t1--;
+            if (t1 ==0)
+            {
+                t1 = ans*4;
+            }
+            txtTime1.Text = t1.ToString();
+        }
+        private void timer5_Tick_1(object sender, EventArgs e)
+        {
+            t2--;
+            if (t2 == 0)
+            {
+                t2 = ans * 4;
+            }
+            txtTime2.Text = t2.ToString();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            t3--;
+            if (t3 == 0)
+            {
+                t3 = ans * 4;
+            }
+            txtTime3.Text = t3.ToString();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            t4--;
+            if (t4 == 0)
+            {
+                t4 = ans * 4;
+            }
+
+            txtTime4.Text = t4.ToString();
+        }
+
+
+        private void txtTime2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtTime3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
